590d7aa16d474c95c30d11f69e9cc05c67402eae
Basit <basitmanzoor438@gmail.com>
2024-10-17 18:17:03 +0530

    adding functionalities to the directory

--- useReducerTask/src/App.jsx
+++ useReducerTask/src/App.jsx
@@ -3,56 +3,71 @@ import EmployeeListJson from "./Components/data.js";
 import "./App.css";
 import EmployeeList from "./Components/EmployeeList/EmployeeList.jsx";
 import TeamList from "./Components/TeamList/TeamList.jsx";
+
 function App() {
-  // const masterData = {
-  //   EmployeeListData: EmployeeListJson,
-  //   TeamListData: [],
-  //   averageAge: 0,
-  // };
-  const reducerfn = (masterData , action) => {
-    console.log(masterData, action);
-    //Add to team lsit
-    if (action.action === "Add_to_team_list") {
-      // masterData.TeamListData.push(payload);
-      return{
-        ...masterData,
-        TeamListData : [...masterData.TeamListData , action.payload]
-      }
-      //Remove from team list
-
-
-    } else if (action.action === "Remove_from_team_list") {
-      console.log("Remove from team list");
-
-      //Calculate average age
-    } else if (action.action === "Calculate_average_age") {
-      console.log("Calculate average age");
-
-      //Sort by age
-
-    } else if (action.action === "Sort_by_age") {
-      console.log("Sort by age");
-    }
-  };
-  const [masterData , dispatch] = useReducer( reducerfn, {
+  // Initialize masterData with employee data and an empty team list
+  const initialState = {
     EmployeeListData: EmployeeListJson,
     TeamListData: [],
     averageAge: 0,
-  });
+  };
 
-  
+  // Reducer function to handle actions
+  const reducerfn = (state, action) => {
+    switch (action.type) {
+      // Add employee to the team list
+      case "Add_to_team_list":
+        return {
+          ...state,
+          TeamListData: [...state.TeamListData, action.payload],
+        };
+      // Remove employee from the team list
+      case "Remove_from_team_list":
+        return {
+          ...state,
+          TeamListData: state.TeamListData.filter(
+            (member) => member.id !== action.payload.id
+          ),
+        };
+      // Calculate average age of team members
+      case "Calculate_average_age":
+        const totalAge = state.TeamListData.reduce((acc, member) => acc + member.age, 0);
+        const averageAge = state.TeamListData.length > 0 ? totalAge / state.TeamListData.length : 0;
+        return {
+          ...state,
+          averageAge: averageAge,
+        };
+      // Sort the team list by age
+      case "Sort_by_age":
+        const sortedTeam = [...state.TeamListData].sort((a, b) => a.age - b.age);
+        return {
+          ...state,
+          TeamListData: sortedTeam,
+        };
+      default:
+        return state;
+    }
+  };
+
+  // useReducer hook to manage the state and dispatch actions
+  const [state, dispatch] = useReducer(reducerfn, initialState);
+
   return (
     <>
       <h1 className="text-3xl font-bold text-center m-5">Employee Directory</h1>
       <div className="app-container flex flex-row gap-4 justify-center mt-5">
-        {" "}
-        {/* Style the container */}
-        <EmployeeList 
-        dispatch={dispatch}
-        data={masterData.EmployeeListData } />
-        <TeamList 
-        dispatch={dispatch}
-        data={masterData.TeamListData} />
+        {/* Employee List component */}
+        <EmployeeList
+          dispatch={dispatch}
+          data={state.EmployeeListData}
+        />
+
+        {/* Team List component */}
+        <TeamList
+          dispatch={dispatch}
+          data={state.TeamListData}
+          averageAge={state.averageAge}
+        />
       </div>
     </>
   );
